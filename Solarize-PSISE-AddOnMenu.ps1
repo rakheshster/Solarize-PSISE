3070bb02eb1b0f4c03d428fd42399cbc405418f6
= <=>
2013-08-20 17:56:05 +0400

    Created a script that adds a menu entry under the Add-ons menu in the ISE to apply Solarize colors

---
+++ Solarize-PSISE-AddOnMenu.ps1
@@ -0,0 +1,12 @@
+﻿# Add a menu entry under the Add-ons menu in PowerShell ISE to apply Solarize colors
+# Clear existing
+$psISE.CurrentPowerShellTab.AddOnsMenu.SubMenus.Clear()
+
+# Create a sub-menu
+$solMenu = $psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add("Solarize",$null,$null)
+
+# Add entries
+$solMenu.Submenus.Add("Apply Dark palette",{.\Solarize-PSISE.ps1 -Dark},"Alt+Shift+D") | Out-Null
+$solMenu.Submenus.Add("Apply Light palette",{.\Solarize-PSISE.ps1},"Alt+Shift+L") | Out-Null
+
+# Documentation: http://technet.microsoft.com/en-us/library/dd819494.aspx
